eab001ee290b827b82a6d8a760987742972ebf68
thomas <thomas.sedlmair@googlemail.com>
2022-08-08 10:39:23 +0200

    implemented on_item_clicked()

--- src/tree_view.ts
+++ src/tree_view.ts
@@ -6,14 +6,27 @@ import * as vscode from 'vscode';
 
 export namespace cwt
 {
+    class line 
+    {
+        readonly text : string;
+        readonly row : number;
+        readonly length : number;
+
+        constructor (text : string, row : number ){
+            this.text = text;
+            this.length = text.length;
+            this.row = row;
+        }
+    }
+
     class tree_item extends vscode.TreeItem 
     {
-        readonly file: string | undefined;
-        readonly line: number | undefined;
+        readonly file: string;
+        readonly line: line;
 
         readonly children: tree_item[] = [];
 
-        constructor(label: string, file: string, line: number) {
+        constructor(label: string, file: string, line: line) {
             super(label, vscode.TreeItemCollapsibleState.None);
             this.file = file;
             this.line = line;
@@ -28,39 +41,49 @@ export namespace cwt
     
     export class tree_view implements vscode.TreeDataProvider<tree_item>
     {
-        private m_data : tree_item [] = [];
-        private m_onDidChangeTreeData: vscode.EventEmitter<tree_item | undefined> = new vscode.EventEmitter<tree_item | undefined>();
-        
-        readonly onDidChangeTreeData ? : vscode.Event<tree_item | undefined> = this.m_onDidChangeTreeData.event;
+        private data : tree_item [] = [];
+        private event_emitter: vscode.EventEmitter<tree_item | undefined> = new vscode.EventEmitter<tree_item | undefined>();
+
+        readonly onDidChangeTreeData ? : vscode.Event<tree_item | undefined> = this.event_emitter.event;
+   
+        private readonly regex_feature = new RegExp("(?<=Feature:).*");
+        private readonly regex_scenario = new RegExp("(?<=Scenario:).*");
+        private readonly regex_scenario_outline = new RegExp("(?<=Scenario Outline:).*");
 
         public constructor()  {
-            vscode.commands.registerCommand('cwt_cucumber.item_clicked', r => this.item_clicked(r));
+            vscode.commands.registerCommand('cwt_cucumber.on_item_clicked', item => this.on_item_clicked(item));
             vscode.commands.registerCommand('cwt_cucumber.refresh', () => this.refresh());
         }
 
-        public getTreeItem(element: tree_item): vscode.TreeItem|Thenable<vscode.TreeItem> {
-            const item = new vscode.TreeItem(element.label!, element.collapsibleState);
-            return item;
+        public getTreeItem(item: tree_item): vscode.TreeItem|Thenable<vscode.TreeItem> {
+            let title = item.label ? item.label.toString() : "";
+            let result = new vscode.TreeItem(title, item.collapsibleState);
+            result.command = { command: 'cwt_cucumber.on_item_clicked', title : title, arguments: [item] };
+            return result;
         }
     
         public getChildren(element : tree_item | undefined): vscode.ProviderResult<tree_item[]> {
-            if (element === undefined) {
-                return this.m_data;
-            } else {
-                return element.children;
-            }
+            return (element === undefined) ? this.data : element.children;
         }
 
-        public item_clicked(item: tree_item) {
-            // we implement this later
+        public on_item_clicked(item: tree_item) {
+            if (item.file === undefined) return;
+            vscode.workspace.openTextDocument(item.file).then( document => {
+                vscode.window.showTextDocument(document).then( editor => {
+                        let pos = new vscode.Position(item.line.row, item.line.length);
+                        editor.selection = new vscode.Selection(pos, pos);
+                        editor.revealRange(new vscode.Range(pos, pos));
+                    }
+                );
+            });
         }
     
         public refresh() {
             // TODO: error/notification no workspace folder opened...
             if (vscode.workspace.workspaceFolders) {
-                this.m_data = [];
+                this.data = [];
                 this.read_directory(vscode.workspace.workspaceFolders[0].uri.fsPath);
-                this.m_onDidChangeTreeData.fire(undefined);
+                this.event_emitter.fire(undefined);
             } 
         }
 
@@ -78,19 +101,22 @@ export namespace cwt
         }
 
         private parse_feature_file(file: string) {
-            const regex_feature = new RegExp("(?<=Feature:).*");
-            const regex_scenario = new RegExp("(?<=Scenario:).*");
-            let reader = rd.createInterface(fs.createReadStream(file))
-            const line_counter = ((i = 0) => () => ++i)();
 
-            reader.on("line", (line : string, line_number : number = line_counter()) => {
-                let is_feature = line.match(regex_feature);
+            let reader = rd.createInterface(fs.createReadStream(file))
+            const line_counter = ((i = 0) => () => i++)();
+
+            reader.on("line", (current_line : string, line_number : number = line_counter()) => {
+                let is_feature = current_line.match(this.regex_feature);
                 if (is_feature) {
-                    this.m_data.push(new tree_item(is_feature[0], file, line_number));
+                    this.data.push(new tree_item(is_feature[0], file, new line(current_line, line_number)));
                 }
-                let is_scenario = line.match(regex_scenario);
+                let is_scenario = current_line.match(this.regex_scenario);
                 if (is_scenario) {
-                    this.m_data.at(-1)?.add_child(new tree_item(is_scenario[0], file, line_number));
+                    this.data.at(-1)?.add_child(new tree_item(is_scenario[0], file, new line(current_line, line_number)));
+                }
+                let is_scenario_outline = current_line.match(this.regex_scenario_outline);
+                if (is_scenario_outline) {
+                    this.data.at(-1)?.add_child(new tree_item(is_scenario_outline[0], file, new line(current_line, line_number)));
                 }
             });
         }
